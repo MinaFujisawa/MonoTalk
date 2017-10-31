0a925b9f590d11d8c90dfc894abf3659b55eeb0d
MinaFujisawa <fujimina37@gmail.com>
2017-10-30 17:47:02 -0700

    add Model.swift

---
+++ MonoTalk/Model.swift
@@ -0,0 +1,50 @@
+//
+//  Item.swift
+//  MonoTalk
+//
+//  Created by MINA FUJISAWA on 2017/10/30.
+//  Copyright © 2017 MINA FUJISAWA. All rights reserved.
+//
+
+import Foundation
+import CoreMedia
+
+struct Category {
+    let uuid : String!
+    var name : String!
+}
+struct Question {
+    let uuid : String!
+    var categoryId : String!
+    let createdDate : Date? // Default questions will be nil
+    var question : String!
+    var exampleAnswer : String?
+    var note : String?
+    var isFavorited : Bool
+    var feeling : Rate
+}
+
+enum Rate {
+    case great
+    case good
+    case soso
+    case notGood
+    case bad
+}
+
+struct RecordAnswer {
+    let uuid : String!
+    let questionId : String!
+    let date : Date!
+    let url : URL!
+    let duration : Float64 //not sure
+    
+//    if let url = Bundle.main.url(forResource: "small", withExtension: "mp4") {
+//        let asset = AVAsset(url: url)
+//
+//        let duration = asset.duration
+//        let durationTime = CMTimeGetSeconds(duration)
+//
+//        print(durationTime)
+//    }
+}
