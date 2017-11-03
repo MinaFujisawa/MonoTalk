81c3154a76c47ff2801d3d452078741676e99720
MinaFujisawa <fujimina37@gmail.com>
2017-11-03 16:04:42 -0700

    add Category List page

---
+++ MonoTalk/CategoryCell.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryCell.swift
+//  MonoTalk
+//
+//  Created by MINA FUJISAWA on 2017/11/03.
+//  Copyright © 2017 MINA FUJISAWA. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        loadNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func loadNib(){
+        let view = Bundle.main.loadNibNamed("CategoryCellXibView", owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+}
