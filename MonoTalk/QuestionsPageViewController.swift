6bf6e0738ceba0f70607c301c7a302bbe92d2dca
MinaFujisawa <fujimina37@gmail.com>
2017-11-06 19:52:35 -0800

    add action sheet for Detail page

--- MonoTalk/QuestionsPageViewController.swift
+++ MonoTalk/QuestionsPageViewController.swift
@@ -10,30 +10,55 @@ import UIKit
 import RealmSwift
 
 class QuestionsPageViewController: UIPageViewController {
-    var selectedIndex : Int!
-    var questions : List<Question>!
-    
+    var startIndex: Int!
+    var currentIndex: Int!
+    var questions: List<Question>?
+    var categoryID : String!
+    var notificationToken: NotificationToken? = nil
+
     var pageCollection = [UIViewController]()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        
-        for i in 0..<questions.count {
-            let detailVC = storyboard.instantiateViewController(withIdentifier: "questionPage") as! QuestionDetailViewController
-            detailVC.question = questions[i]
-            pageCollection.append(detailVC)
-        }
-
-        self.title = String(format: "%d / %d", selectedIndex+1, pageCollection.count)
-        self.setViewControllers([pageCollection[selectedIndex]], direction: .forward, animated: true, completion: nil)
         self.dataSource = self
         self.delegate = self
+        
+        let realm = try! Realm()
+        let category = realm.object(ofType: Category.self, forPrimaryKey: categoryID)
+        questions = category?.questions
+        
+        // MARK:Observe Results Notifications
+        notificationToken = questions?.observe { changes in
+            switch changes {
+            case .initial:
+                print("initial")
+            case .update(_, let deletions, let insertions, let modifications):
+                print("update")
+            case .error(let error):
+                fatalError("\(error)")
+            }
+        }
+        setUp()
+        
     }
     
-    
+    func setUp(){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // Create pageCollection
+        if let questions = questions {
+            for i in 0..<questions.count {
+                let detailVC = storyboard.instantiateViewController(withIdentifier: "questionPage") as! QuestionDetailViewController
+                detailVC.question = questions[i]
+                pageCollection.append(detailVC)
+            }
+        }
+        
+        self.title = String(format: "%d / %d", startIndex + 1, pageCollection.count)
+        currentIndex = startIndex
+        self.setViewControllers([pageCollection[startIndex]], direction: .forward, animated: true, completion: nil)
+    }
 }
 
 extension QuestionsPageViewController: UIPageViewControllerDataSource {
@@ -54,6 +79,42 @@ extension QuestionsPageViewController: UIPageViewControllerDataSource {
         }
         return pageCollection[index + 1]
     }
+
+    @IBAction func menuButton(_ sender: Any) {
+        let alert: UIAlertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let edit = UIAlertAction(title: "Edit", style: .default, handler: {
+            (action: UIAlertAction!) -> Void in
+            self.goToEdit()
+        })
+
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        let delete: UIAlertAction = UIAlertAction(title: "Delete", style: .destructive, handler: {
+            (action: UIAlertAction!) -> Void in
+            //
+        })
+
+        alert.addAction(edit)
+        alert.addAction(cancel)
+        alert.addAction(delete)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func goToEdit() {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let editVC = storyBoard.instantiateViewController(withIdentifier: "EditQuestion") as! AddQuestionViewController
+        editVC.existingQuestion = self.questions?[self.currentIndex]
+
+        // add animation (bottom to top)
+//        let transition:CATransition = CATransition()
+//        transition.duration = 0.5
+//        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+//        transition.type = kCATransitionPush
+//        transition.subtype = kCATransitionFromBottom
+//        self.navigationController!.view.layer.add(transition, forKey: kCATransition)
+
+        self.navigationController?.pushViewController(editVC, animated: true)
+    }
 }
 
 extension QuestionsPageViewController: UIPageViewControllerDelegate {
@@ -62,6 +123,7 @@ extension QuestionsPageViewController: UIPageViewControllerDelegate {
         // Add Title
         guard let viewController = self.viewControllers?.last else { return }
         guard let index = pageCollection.index(of: viewController) else { return }
-        self.title = String(format: "%d / %d", index + 1, pageCollection.count)
+        currentIndex = index
+        self.title = String(format: "%d / %d", currentIndex + 1, pageCollection.count)
     }
 }
