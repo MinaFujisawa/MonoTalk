136fbf8b67510537924eaa335e150b1769b4e788
MinaFujisawa <fujimina37@gmail.com>
2017-11-06 22:40:58 -0800

    add remove Question func

--- MonoTalk/QuestionsPageViewController.swift
+++ MonoTalk/QuestionsPageViewController.swift
@@ -15,16 +15,17 @@ class QuestionsPageViewController: UIPageViewController {
     var questions: List<Question>?
     var categoryID: String!
     var notificationToken: NotificationToken? = nil
+    var realm: Realm!
 
     var pageCollection = [UIViewController]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.dataSource = self
         self.delegate = self
 
-        let realm = try! Realm()
+        // Fetch questions data
+        realm = try! Realm()
         let category = realm.object(ofType: Category.self, forPrimaryKey: categoryID)
         questions = category?.questions
 
@@ -43,9 +44,68 @@ class QuestionsPageViewController: UIPageViewController {
             }
         }
 
-        self.title = String(format: "%d / %d", startIndex + 1, pageCollection.count)
+        setTitle(index: currentIndex)
         currentIndex = startIndex
         self.setViewControllers([pageCollection[startIndex]], direction: .forward, animated: true, completion: nil)
+    }
+
+    // MARK: Action sheet
+    @IBAction func menuButton(_ sender: Any) {
+        let alert: UIAlertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let edit = UIAlertAction(title: "Edit", style: .default, handler: {
+            (action: UIAlertAction!) -> Void in
+            self.performSegue(withIdentifier: "GoToEdit", sender: self)
+        })
+
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        let delete: UIAlertAction = UIAlertAction(title: "Delete", style: .destructive, handler: {
+            (action: UIAlertAction!) -> Void in
+            self.remove()
+        })
+
+        alert.addAction(edit)
+        alert.addAction(cancel)
+        alert.addAction(delete)
+        present(alert, animated: true, completion: nil)
+    }
+
+    func remove() {
+        // Reove
+        try! self.realm.write {
+            realm.delete(questions![currentIndex])
+        }
+        pageCollection.remove(at: currentIndex)
+
+        // Page optimize
+        if currentIndex - 1 > 0 {
+            self.setViewControllers([pageCollection[currentIndex - 1]], direction: .reverse, animated: false, completion: nil)
+            currentIndex = currentIndex - 1
+            setTitle(index: currentIndex)
+        } else {
+            if pageCollection.count > 0 {
+                self.setViewControllers([pageCollection[currentIndex]], direction: .forward, animated: false, completion: nil)
+                setTitle(index: currentIndex)
+            } else {
+                // No item
+                _ = navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+
+    func setTitle(index: Int) {
+        self.title = String(format: "%d / %d", index + 1, pageCollection.count)
+    }
+
+    // MARK: segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "GoToEdit" {
+            let nav = segue.destination as! UINavigationController
+            let editVC = nav.topViewController as! AddEditQuestionViewController
+            if let questions = questions {
+                editVC.existingQuestion = questions[self.currentIndex]
+            }
+        }
     }
 }
 
@@ -67,36 +127,6 @@ extension QuestionsPageViewController: UIPageViewControllerDataSource {
         }
         return pageCollection[index + 1]
     }
-
-    @IBAction func menuButton(_ sender: Any) {
-        let alert: UIAlertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        let edit = UIAlertAction(title: "Edit", style: .default, handler: {
-            (action: UIAlertAction!) -> Void in
-            self.performSegue(withIdentifier: "GoToEdit", sender: self)
-        })
-
-        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-
-        let delete: UIAlertAction = UIAlertAction(title: "Delete", style: .destructive, handler: {
-            (action: UIAlertAction!) -> Void in
-            //
-        })
-
-        alert.addAction(edit)
-        alert.addAction(cancel)
-        alert.addAction(delete)
-        present(alert, animated: true, completion: nil)
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "GoToEdit" {
-            let nav = segue.destination as! UINavigationController
-            let editVC = nav.topViewController as! AddEditQuestionViewController
-            if let questions = questions {
-                editVC.existingQuestion = questions[self.currentIndex]
-            }
-        }
-    }
 }
 
 extension QuestionsPageViewController: UIPageViewControllerDelegate {
@@ -106,6 +136,6 @@ extension QuestionsPageViewController: UIPageViewControllerDelegate {
         guard let viewController = self.viewControllers?.last else { return }
         guard let index = pageCollection.index(of: viewController) else { return }
         currentIndex = index
-        self.title = String(format: "%d / %d", currentIndex + 1, pageCollection.count)
+        setTitle(index: currentIndex)
     }
 }
