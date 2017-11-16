905c7c6bad4721c11cc6459e8344ae891029fd2a
MinaFujisawa <fujimina37@gmail.com>
2017-11-15 19:56:26 -0800

    fix number badge to display more than 1 digit

--- MonoTalk/Presentation/CategoryList/CategoryTableViewCell.swift
+++ MonoTalk/Presentation/CategoryList/CategoryTableViewCell.swift
@@ -9,24 +9,24 @@
 import UIKit
 
 class CategoryTableViewCell: UITableViewCell {
-    
+
     @IBOutlet weak var numTextView: UITextView!
     @IBOutlet weak var nameLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         loadNib()
-        
+
         // UI
         nameLabel.textColor = MyColor.darkText.value
         nameLabel.font = UIFont.systemFont(ofSize: TextSize.normal.rawValue)
-        
+
         numTextView.textColor = MyColor.theme.value
-        numTextView.textContainerInset = UIEdgeInsetsMake(1, 0, 0, 0);
+        numTextView.textContainerInset = UIEdgeInsetsMake(1, 3, 0, 3)
         numTextView.font = UIFont.systemFont(ofSize: 14)
         numTextView.backgroundColor = MyColor.paledTheme.value
         numTextView.layer.cornerRadius = numTextView.frame.height / 2
         numTextView.layer.masksToBounds = true
-        
+
         self.preservesSuperviewLayoutMargins = false
         self.separatorInset = UIEdgeInsets.zero
         self.layoutMargins = UIEdgeInsets.zero
@@ -37,11 +37,11 @@ class CategoryTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
-    func loadNib(){
+
+    func loadNib() {
         let view = Bundle.main.loadNibNamed("CategoryCellXib", owner: self, options: nil)?.first as! UIView
         view.frame = CGRect(x: self.bounds.origin.x, y: self.bounds.origin.y, width: UIScreen.main.bounds.width, height: self.bounds.height)
         self.addSubview(view)
     }
-    
+
 }
